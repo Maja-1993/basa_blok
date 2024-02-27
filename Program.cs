224b0147f6aff08cd2a39accbf83297abcc45bec
Maja <mmsch-ka@mail.ru>
2024-02-27 23:36:26 +0400

    Заполняем новый массив и вывод

--- Program.cs
+++ Program.cs
@@ -57,3 +57,19 @@ string[] GenerateNewArray(string[] workArray)
     }
     return resultArray;
 }
+
+string[] GenerateNewArray(string[] workArray)
+{
+    int resultArrayLength = CountStringSymbols(workArray);
+    string[] resultArray = new string[resultArrayLength];
+    int i = 0;
+    foreach (string item in workArray)
+    {
+        if (item.Length <= 3)
+        {
+            resultArray[i] = item;
+            i++;
+        }
+    }
+    return resultArray;
+}

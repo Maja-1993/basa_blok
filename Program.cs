a951cac11ae9285b3d08735a0374b585c5b6496e
Maja <mmsch-ka@mail.ru>
2024-02-27 23:18:00 +0400

    Cоздаем массив

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿﻿string[] workArray = FillArray();
+string[] resultArray = GenerateNewArray(workArray);
+string firstArray = PrintArray(workArray);
+string secondArray = PrintArray(resultArray);
+Console.WriteLine(firstArray + " -> " + secondArray);
+
+string[] FillArray()

7d5a5d79c297b467ca866b8f97aaeb0a38b668a4
Maja <mmsch-ka@mail.ru>
2024-02-27 23:27:06 +0400

    Наполняем массив строками

--- Program.cs
+++ Program.cs
@@ -5,3 +5,11 @@ string secondArray = PrintArray(resultArray);
 Console.WriteLine(firstArray + " -> " + secondArray);
 
 string[] FillArray()
+{
+    Console.WriteLine("Введите данные через пробел, по окончании ввода нажмите Enter: ");
+    string? enterSymbols = Console.ReadLine();
+    if (enterSymbols == null) { enterSymbols = ""; };
+    char[] separators = new char[] { ',', ' ' };
+    string[] workArray = enterSymbols.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+    return workArray;
+}
